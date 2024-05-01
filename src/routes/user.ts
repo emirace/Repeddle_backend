0bcb9d6eaf3cf43ff22945b4db5f49cf807b83d6
emirace <emmanuelakwuba57@gmail.com>
2024-05-01 11:03:21 +0100

    add verify email

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,38 +1,40 @@
 // userRoutes.ts
-import express from 'express';
-import UserController from '../controller/user';
-import { authorize } from '../middleware/user';
+import express from "express";
+import UserController from "../controller/user";
+import { authorize } from "../middleware/user";
 
 const router = express.Router();
 
-router.post('/send-verification-email', UserController.sendVerificationEmail);
+router.post("/send-verification-email", UserController.sendVerificationEmail);
 
-router.post('/send-verification-email', UserController.sendVerificationEmail);
+router.post("/send-verification-email", UserController.sendVerificationEmail);
 
-router.post('/forgot-password', UserController.forgotPassword);
+router.post("/forgot-password", UserController.forgotPassword);
 
-router.post('/register', UserController.register);
+router.post("/register", UserController.register);
 
 // Login user
-router.post('/login', UserController.login);
+router.post("/login", UserController.login);
 
 // Get user profile
-router.get('/profile', authorize(), UserController.getProfile);
+router.get("/profile", authorize(), UserController.getProfile);
 
 // Update user profile
-router.put('/profile', authorize(), UserController.updateProfile);
+router.put("/profile", authorize(), UserController.updateProfile);
 
-router.post('/suggested-username', UserController.getSuggestedUsername);
+router.post("/suggested-username", UserController.getSuggestedUsername);
 
-router.get('/top-sellers', UserController.getTopSellers);
+router.get("/top-sellers", UserController.getTopSellers);
 
-router.post('/follow/:userId', authorize(), UserController.followUser);
+router.post("/follow/:userId", authorize(), UserController.followUser);
 
-router.get('/:username', UserController.getUserByUsername);
+router.get("/:username", UserController.getUserByUsername);
 
-router.delete('/unfollow/:userId', authorize(), UserController.unfollowUser);
+router.get("verify-email/:token", UserController.verifyEmail);
+
+router.delete("/unfollow/:userId", authorize(), UserController.unfollowUser);
 
 // Delete user account
-router.delete('/profile', authorize(), UserController.deleteAccount);
+router.delete("/profile", authorize(), UserController.deleteAccount);
 
 export default router;
