98f273ab052bcc3b76bdcfbb5b55ef48dae9738f
emirace <emmanuelakwuba57@gmail.com>
2025-06-19 16:17:46 +0100

    up

--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -1,24 +1,28 @@
 // swaggerConfig.ts
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerSchema from './swaggerDocs/model';
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerSchema from "./swaggerDocs/model";
 
 const swaggerOptions = {
   swaggerDefinition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Repeddle backend Documentation',
-      version: '1.0.0',
-      description: 'API Documentation generated with Swagger',
+      title: "Repeddle backend Documentation",
+      version: "1.0.0",
+      description: "API Documentation generated with Swagger",
     },
     // basePath: '/',
     servers: [
       {
-        url: 'http://localhost:5000/api', // Development server base URL
-        description: 'Development Server',
+        url: "http://localhost:5000/api", // Development server base URL
+        description: "Development Server",
       },
       {
-        url: 'https://repeddle-backend.onrender.com/api', // Production server base URL
-        description: 'Production Server',
+        url: "https://repeddle-backend.onrender.com/api", // Production server base URL
+        description: "Production Server",
+      },
+      {
+        url: "https://test.repeddle.com/api",
+        description: "Test Production Server",
       },
     ],
     components: {
@@ -28,15 +32,15 @@ const swaggerOptions = {
           // name: 'Authorization',
           // scheme: 'bearer',
           // in: 'header',
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
       schemas: swaggerSchema,
     },
   },
-  apis: ['./src/routes/*.ts', './src/swaggerDocs/*.ts'], // Specify the path to your route files
+  apis: ["./src/routes/*.ts", "./src/swaggerDocs/*.ts"], // Specify the path to your route files
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
